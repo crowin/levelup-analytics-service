ff8c25948db71eea64ed9711526b4d2c79bf5b99
Alexander Stolnikov <crowin@users.noreply.github.com>
2020-10-13 23:28:13 +0300

    project init

---
+++ src/test/java/ru/levelup/bank/analytics/AnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.levelup.bank.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
